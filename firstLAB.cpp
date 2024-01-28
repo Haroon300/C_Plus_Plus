c88613510cb1616eabcd4362efd44afeb5e419fd
Haroon300 <haroonhammad300@gmail.com>
2024-01-28 19:16:32 +0500

    lab 1 task no 01

--- firstLAB.cpp
+++ firstLAB.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+#include <cmath>
 
-int main(){
+int main()
+{
+
     int numb1, numb2, sum;
+    double root;
 
-    std::count<<"Enter 1st Number\n";
-    std::cin>>numb1;
-    
-    std::count<<"Enter 2nd Number\n";
-    std::cin>>numb2;
+    std::count << "Enter 1st Number\n";
+    std::cin >> numb1;
 
-    sum = numb1+numb2;
+    std::count << "Enter 2nd Number\n";
+    std::cin >> numb2;
 
-    std::count<<"Sum of Number is">>sum;
+    sum = numb1 + numb2;
 
+    root = sqrt(sum);
+
+    std::count << "Sum of Number is : " << sum << "\n";
+    std::count << "Square root of sum is  : " << root << "\n";
 
     return 0;
 }
