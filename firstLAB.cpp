d750201502e333c9899832723deda89ba4a297bb
Haroon300 <haroonhammad300@gmail.com>
2024-01-28 18:20:48 +0500

    First Lab of  C++

---
+++ firstLAB.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    int numb1, numb2, sum;
+
+    std::count<<"Enter 1st Number\n";
+    std::cin>>numb1;
+    
+    std::count<<"Enter 2nd Number\n";
+    std::cin>>numb2;
+
+    sum = numb1+numb2;
+
+    std::count<<"Sum of Number is">>sum;
+
+
+    return 0;
+}
